8c3fa66d7e164468c9b3d4672f08a02dad1f8471
Otis Pham <efert269@gmail.com>
2022-12-17 18:23:36 +0700

    fix: config completion open ai

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -165,11 +165,11 @@ async function handleChatGPTOpenAI(message) {
     const { data } = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${message}`,
-      temperature: process.env.TEMPERATURE_RESPONSE_OPENAI || 0.5,
-      max_tokens: process.env.MAX_TOKENS_RESPONSE_OPENAI || 60,
-      top_p: process.env.TOP_P_RESPONSE_OPENAI || 0.3,
-      frequency_penalty: process.env.FREQUENCY_PENALTY_RESPONSE_OPENAI || 0.5,
-      presence_penalty: process.env.PRESENCE_PENALTY_RESPONSE_OPENAI || 0.0,
+      temperature: parseFloat(process.env.TEMPERATURE_RESPONSE_OPENAI) || 0.5,
+      max_tokens: parseFloat(process.env.MAX_TOKENS_RESPONSE_OPENAI) || 60,
+      top_p: parseFloat(process.env.TOP_P_RESPONSE_OPENAI) || 0.3,
+      frequency_penalty: parseFloat(process.env.FREQUENCY_PENALTY_RESPONSE_OPENAI) || 0.5,
+      presence_penalty: parseFloat(process.env.PRESENCE_PENALTY_RESPONSE_OPENAI) || 0.0,
     });
     console.log('> handleChatGPTOpenAI response:', data.choices[0].text);
     return data?.choices[0]?.text || "Error! An error occurred. Please contact me m.me/hnam.se"
