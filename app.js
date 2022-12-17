d6fecd746d63232d71c60005b506aa1526014684
Otis Pham <efert269@gmail.com>
2022-12-17 16:13:06 +0700

    feat: add logger

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const webhookRouter = require('./src/apis');
 const app = express()
 require('dotenv').config()
+const morgan = require('morgan')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('common'))
+
 
 app.get('/', (req, res) => {
   res.send('hello from simple server :)')
